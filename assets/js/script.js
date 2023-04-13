e5e021a0f57748d0504ea2c979debd5234ae587f
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-04-12 22:19:53 -0500

    Added the date/time and the color change function for each hour slot

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,45 @@
+// Job 1, add current date in the header. The area has an ID of #currentDay
+// Job 2 Create 6 more blocks of time by copy pasting in the HTML
+// !Job 3 using dayjs? jquery? google? add and remove classes to the appropriate blocks of time
+// !Job 4 Clicking on a block of time will allow me to input data
+// !Job 5 have the save button save the entered event to local storage.  When the page is refreshed the saved events persist
+
+// TODO use dayjs to create a current day in the header.
+
+let dayTime = dayjs().format('dddd, MMMM D YYYY');
+$('#currentDay').text(dayTime);
+
+
+//function to loop through each elementID that begins with 'hour-'
+// because the IDs are labeled with the corresponding hour, the fucntion turns the id (eg. hour-15) into an array of 2 parts, exluding the "-".  (.split does this)
+//it then takes the 2nd element of that array [1] and reads it as an integer (parseInt).  
+//if that value is < = or > that the current hour as determined by dayjs, the function adds/removes classes as necessary.
+function changeColor() {
+  let currentTime = dayjs();  
+  let currentHour=currentTime.hour();
+  let elementToChange=$("[id^='hour-']");
+  elementToChange.remove("past present future");
+
+  elementToChange.each(function() {
+    let hour = parseInt($(this).attr("id").split("-")[1]);
+    if (hour < currentHour) {
+      $(this).addClass("past");
+    } else if (hour === currentHour) {
+      $(this).addClass("present");
+    } else $(this).addClass("future");
+  })
+}
+
+changeColor();
+setInterval(changeColor, 5000);
+
+
+
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
