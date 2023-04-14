7a3d708aa4804ddffc44e803024cf84e94e77ebe
Bradfh <128874401+Bradfh@users.noreply.github.com>
2023-04-13 21:01:55 -0500

    Small edits done while out and about

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,26 @@
 // Job 1, add current date in the header. The area has an ID of #currentDay
 // Job 2 Create 6 more blocks of time by copy pasting in the HTML
-// !Job 3 using dayjs? jquery? google? add and remove classes to the appropriate blocks of time
+// Job 3 using dayjs? jquery? google? add and remove classes to the appropriate blocks of time
 // !Job 4 Clicking on a block of time will allow me to input data
 // !Job 5 have the save button save the entered event to local storage.  When the page is refreshed the saved events persist
+// !Job 6 move all of these functions into the provided function
 
-// TODO use dayjs to create a current day in the header.
-
+//display date/time on top
 let dayTime = dayjs().format('dddd, MMMM D YYYY');
 $('#currentDay').text(dayTime);
 
 
 //function to loop through each elementID that begins with 'hour-'
-// because the IDs are labeled with the corresponding hour, the fucntion turns the id (eg. hour-15) into an array of 2 parts, exluding the "-".  (.split does this)
+// because the IDs are labeled with the corresponding numerical hour, the function turns the id (eg. hour-15) into an array of 2 parts, excluding the "-".  (.split does this)
 //it then takes the 2nd element of that array [1] and reads it as an integer (parseInt).  
 //if that value is < = or > that the current hour as determined by dayjs, the function adds/removes classes as necessary.
+// because the function is looping through every element with the id of hour-, (this) makes it point at the element it is currently on
 function changeColor() {
   let currentTime = dayjs();  
   let currentHour=currentTime.hour();
   let elementToChange=$("[id^='hour-']");
   elementToChange.remove("past present future");
+  console.log(currentTime); //logging time every 5s is working!
 
   elementToChange.each(function() {
     let hour = parseInt($(this).attr("id").split("-")[1]);
@@ -32,6 +34,10 @@ function changeColor() {
 
 changeColor();
 setInterval(changeColor, 5000);
+
+// TODO create a var for input into localstorage.  The storage key needs to match (this?) the id of the box where it was entered.
+// TODO use DOM traversal to get the appropriate #id.  I can probably use/save the id as well so that when it is parsed it will go back into the correct container as well.
+
 
 
 
